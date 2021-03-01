f58323e3c1ea465ad277ffa51c281f2bf4074736
Travis CI User <travis@example.org>
2021-03-01 14:59:59 +0000

    Update da deps 💍🦷🫀

---
+++ js/2.9c5968d4a6b73c5991d9.js
@@ -0,0 +1,2 @@
+(self.webpackChunktrust_frontend=self.webpackChunktrust_frontend||[]).push([[2],{93002:(e,n,t)=>{"use strict";t.r(n);var r=t(94812),a=t(16354),o=t(97522),l=t.n(o),s=t(16530),c=t(35240),u=t(31806);const m=function(){return l().createElement(a.Pj,{locale:navigator.language.slice(0,2),messages:u,onError:console.log},l().createElement(s.BrowserRouter,{basename:(0,c.eb)(window.location.pathname)},l().createElement(r.Z,null)))};var d=t(69770);t.n(d)().render(l().createElement(m,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=2.9c5968d4a6b73c5991d9.js.map
