72036ac1cfb5f479a7d1a846de8a70390f1fd4b6
Travis CI User <travis@example.org>
2021-06-01 18:49:42 +0000

    Update deps 🐊🍟🛹 (#28)

--- js/2.7b52ff5c19441470453c.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktrust_frontend=self.webpackChunktrust_frontend||[]).push([[2],{93002:(e,n,t)=>{"use strict";t.r(n);var r=t(94812),a=t(16354),o=t(93264),l=t.n(o),s=t(16530),c=t(35240),u=t(31806);const m=function(){return l().createElement(a.Pj,{locale:navigator.language.slice(0,2),messages:u,onError:console.log},l().createElement(s.BrowserRouter,{basename:(0,c.eb)(window.location.pathname)},l().createElement(r.Z,null)))};var d=t(3644);t.n(d)().render(l().createElement(m,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=2.7b52ff5c19441470453c.js.map
