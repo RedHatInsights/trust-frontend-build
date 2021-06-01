b34d64100d308371a3a71ae2a79020ea839021b5
Travis CI User <travis@example.org>
2021-06-01 18:48:16 +0000

    Update deps 🐊🍟🛹 (#28)

---
+++ js/412.87a284ec101b72eb2094.js
@@ -0,0 +1,2 @@
+(self.webpackChunktrust_frontend=self.webpackChunktrust_frontend||[]).push([[412],{8412:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>f});var r=t(7999),a=t(6354),o=t(3264),l=t.n(o),s=t(6530),c=t(5240),u=t(1806);const f=function(){return l().createElement(a.Pj,{locale:navigator.language.slice(0,2),messages:u,onError:console.log},l().createElement(s.BrowserRouter,{basename:(0,c.eb)(window.location.pathname)},l().createElement(r.Z,null)))}}}]);
+//# sourceMappingURL=../sourcemaps/412.76a69854ee04acd3d0c8.js.map
