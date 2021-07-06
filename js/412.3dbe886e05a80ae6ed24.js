4b3d754e46d9ca6d9d4451f3a96262cd269766be
Travis CI User <travis@example.org>
2021-07-06 14:04:58 +0000

    Update deps 🪂🥸🥳

--- js/412.3dbe886e05a80ae6ed24.js
+++ js/412.3dbe886e05a80ae6ed24.js
@@ -1,2 +1,2 @@
 (self.webpackChunktrust_frontend=self.webpackChunktrust_frontend||[]).push([[412],{8412:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>f});var r=t(7999),a=t(6354),o=t(3264),l=t.n(o),s=t(6530),c=t(5240),u=t(1806);const f=function(){return l().createElement(a.Pj,{locale:navigator.language.slice(0,2),messages:u,onError:console.log},l().createElement(s.BrowserRouter,{basename:(0,c.eb)(window.location.pathname)},l().createElement(r.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.a9446eea8262540a6f87.js.map
+//# sourceMappingURL=../sourcemaps/412.79b42d657626b592d99d.js.map
