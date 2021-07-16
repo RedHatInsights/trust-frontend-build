06643be2b0d8feace1069c1cf6a937e48b3b3f0c
Travis CI User <travis@example.org>
2021-07-16 17:00:17 +0000

    Cloud -> Console, update deps, update webpack config (#29)
    fixes https://issues.redhat.com/browse/ADVISOR-1851

--- js/491.9cf1809f2b1ceee48d4d.js
+++ js/491.9cf1809f2b1ceee48d4d.js
@@ -1,2 +1,2 @@
 (self.webpackChunktrust_frontend=self.webpackChunktrust_frontend||[]).push([[491],{6491:(e,n,t)=>{"use strict";t.r(n);var r=t(7999),a=t(6354),o=t(3264),l=t.n(o),s=t(6530),c=t(5240),u=t(1806);const m=function(){return l().createElement(a.Pj,{locale:navigator.language.slice(0,2),messages:u,onError:console.log},l().createElement(s.BrowserRouter,{basename:(0,c.eb)(window.location.pathname)},l().createElement(r.Z,null)))};var d=t(3644);t.n(d)().render(l().createElement(m,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.79b42d657626b592d99d.js.map
+//# sourceMappingURL=../sourcemaps/491.01be2a024ceef2179b5c.js.map
